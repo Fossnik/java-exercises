9678fa1b82955a757d54fba389f877d5f13eaf1e
Zack Hartmann <zackhartmann@gmail.com>
2017-12-27 18:51:56 -0500

    First test Pass

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,11 @@
 class ReverseString {
+		String reverse(String inputString) {
+			String reversed = new String("");
 
-    String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-  
+			for (int i = inputString.length(); i > 0; i--) {
+			  reversed += inputString.charAt(i);
+			}
+
+			return reversed;
+		}
 }
