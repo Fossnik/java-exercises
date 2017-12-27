c9f0b02bec778bc73e696828c9237bd8148816a5
Zack Hartmann <zackhartmann@gmail.com>
2017-12-27 18:59:32 -0500

    Revised Reversal Logic

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -2,7 +2,7 @@ class ReverseString {
 		String reverse(String inputString) {
 			String reversed = new String("");
 
-			for (int i = inputString.length(); i > 0; i--) {
+			for (int i = inputString.length() - 1; i >= 0; i--) {
 			  reversed += inputString.charAt(i);
 			}
 
