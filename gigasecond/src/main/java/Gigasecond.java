fc31fc38b05b0341b52d06b746a19ccfbbbdb7a7
Zack Hartmann <zackhartmann@gmail.com>
2017-12-28 16:57:19 -0500

    Implemented method to resolve LocalDateTime from the LocalDate using atStartOfDay method

--- gigasecond/src/main/java/Gigasecond.java
+++ gigasecond/src/main/java/Gigasecond.java
@@ -2,17 +2,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 class Gigasecond {
+	protected LocalDateTime birthDateTime;
 
-    Gigasecond(LocalDate birthDate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Gigasecond(LocalDate birthDate) {
+		// atStartOfDay() constructs a LocalDateTime from a LocalDate
+		// LocalDate is useless because of it's imprecision.
+		this.birthDateTime = birthDate.atStartOfDay();
+	}
 
-    Gigasecond(LocalDateTime birthDateTime) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Gigasecond(LocalDateTime birthDateTime) {
+		this.birthDateTime = birthDateTime;
+	}
 
-    LocalDateTime getDate() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	// calculate the billionth second using LocalDateTime method
+	LocalDateTime getDate() {
+		return birthDateTime.plusSeconds(1000000000);
+	}
 
 }
