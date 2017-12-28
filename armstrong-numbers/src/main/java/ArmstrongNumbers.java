d6820d89d90aaa33452b01edbd9bad9934c50f99
Zack Hartmann <zackhartmann@gmail.com>
2017-12-27 20:13:01 -0500

    Implemented Armstrong Number Detection

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -2,8 +2,26 @@ class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
 
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-		
+		// resolve how many digits comprise the integer
+		int numDigits = 0;
+
+		for (int i = 1; numberToCheck / i > 1; i *= 10) {
+			numDigits++;
+		}
+
+		// sum all of the digits
+		int sum = 0;
+		int remainder = numberToCheck;
+
+		// iterate through each digit
+		while (remainder > 1) {
+			// sum each digit's exponential
+			sum += Math.pow(remainder % 10, numDigits);
+			remainder /= 10;
+		}
+
+		// returns true if the number is an armstrong number
+		return sum == numberToCheck;
 	}
 
 }
