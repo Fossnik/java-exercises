79bc0ef2c2c2391eabe6915186b621d37e013790
Zack Hartmann <zackhartmann@gmail.com>
2017-12-28 14:23:00 -0500

    Implemented case insensitivity

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,20 @@
 public class PangramChecker {
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	public boolean isPangram(String input) {
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+		// redefine alphabet sans any chars found within input
+		for (int i = 0; i < input.length(); i++) {
+			alphabet = alphabet.replace(input.toLowerCase().charAt(i), ' ');
+		}
+
+		// test whether the alphabet string has been cleared
+		for (int i = 0; i < alphabet.length(); i++) {
+			if (alphabet.charAt(i) != ' ')
+				return false;
+		}
+
+		return true;
+	}
 
 }
