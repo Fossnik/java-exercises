5c721298d00b95b6f1a72b6f7359aaedfd4ab243
Zack Hartmann <zackhartmann@gmail.com>
2017-12-28 17:56:53 -0500

    Implemented Dash Character Checking

--- acronym/src/main/java/Acronym.java
+++ acronym/src/main/java/Acronym.java
@@ -1,11 +1,24 @@
 class Acronym {
 
-    Acronym(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	protected String phrase;
+	protected String acronym = "";
 
-    String get() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Acronym(String phrase) {
+		this.phrase = phrase;
+	}
+
+	String get() {
+		// assume the first character will be the first initial in the acronym
+		acronym += phrase.toUpperCase().charAt(0);
+
+		// loop through phrase
+		for (int i = 1; i <= phrase.length() - 1; i++) {
+			// if preceding char is a space (or dash), add the initial to the acronym
+			if (phrase.charAt(i - 1) == ' ' || phrase.charAt(i - 1) == '-')
+				acronym += phrase.toUpperCase().charAt(i);
+		}
+
+		return acronym;
+	}
 
 }
