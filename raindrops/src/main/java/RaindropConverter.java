a4a5be12506fe9c1a9e00500e1b91fe974625c26
Zack Hartmann <zackhartmann@gmail.com>
2017-12-28 19:50:14 -0500

    Implemented Combinatorial Output

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,16 @@
 class RaindropConverter {
+	private String out = "";
 
-    String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	String convert(int number) {
+		// use the modulus to determine factorability.
+		if (number % 3 == 0) out += "Pling";
+		if (number % 5 == 0) out += "Plang";
+		if (number % 7 == 0) out += "Plong";
+
+		if (out.length() > 0)
+			return out;
+
+		return Integer.toString(number);
+	}
 
 }
