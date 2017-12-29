33309a9a072cf0354d73d9cece98058b8fc2491a
Zack Hartmann <zackhartmann@gmail.com>
2017-12-28 19:35:27 -0500

    Annotations

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,15 +1,17 @@
 class Scrabble {
 
-	// Letter                           Value
-	// A, E, I, O, U, L, N, R, S, T       1
-	// D, G                               2
-	// B, C, M, P                         3
-	// F, H, V, W, Y                      4
-	// K                                  5
-	// J, X                               8
-	// Q, Z                               10
+/*
+	Letter                           Value
+	A, E, I, O, U, L, N, R, S, T       1
+	D, G                               2
+	B, C, M, P                         3
+	F, H, V, W, Y                      4
+	K                                  5
+	J, X                               8
+	Q, Z                               10
+**/
 
-	protected String word;
+	private String word;
 
 	Scrabble(String word) {
 		this.word = word.toUpperCase();
@@ -37,6 +39,7 @@ class Scrabble {
 				case 'L': case 'N':	case 'R': case 'S': case 'T':
 					wordScore++; // 1
 				default:
+					continue;
 			}
 		}
 
