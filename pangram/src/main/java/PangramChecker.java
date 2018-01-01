dda35ecbed5a8534e337d50026de3f4ef5e10ad5
Zack Hartmann <zackhartmann@gmail.com>
2018-01-01 14:30:25 -0500

    Implemented simplified algorithm with ASCII

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,16 +1,14 @@
 public class PangramChecker {
 
 	public boolean isPangram(String input) {
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		// instead of manually defining the 26 alphabetical letters, we can
+		// utilize ASCII, which already encapsulates that alphabet sequence.
+		// this can be implemented through casting likeso:
+		// int theNumberOfLetterA = (int)'a'; // 97th char in ASCII
 
-		// redefine alphabet sans any chars found within input
-		for (int i = 0; i < input.length(); i++) {
-			alphabet = alphabet.replace(input.toLowerCase().charAt(i), ' ');
-		}
-
-		// test whether the alphabet string has been cleared
-		for (int i = 0; i < alphabet.length(); i++) {
-			if (alphabet.charAt(i) != ' ')
+		// iterate - seek an instance of each alphabetical within our input string
+		for (int alpha = 'a'; alpha <= 'z'; alpha++) {
+			if (input.toLowerCase().indexOf(alpha) == -1)
 				return false;
 		}
 
