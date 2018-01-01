cd7de96d1c0690f1b2d1251dbdc35955d0bbbfb5
Zack Hartmann <zackhartmann@gmail.com>
2018-01-01 14:43:56 -0500

    Simplified for loop - defined scope of variables

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,11 +1,12 @@
 class Hamming {
-	String leftStrand;
-	String rightStrand;
+	private String leftStrand;
+	private String rightStrand;
 
 	Hamming(String leftStrand, String rightStrand) {
 		if (leftStrand.length() != rightStrand.length())
 			throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
 
+    // given String inputs define these values within the class object
 		this.leftStrand = leftStrand;
 		this.rightStrand = rightStrand;
 	}
@@ -13,7 +14,7 @@ class Hamming {
 	int getHammingDistance() {
 		int hammingDistance = 0;
 
-		for (int i = 0; i <= leftStrand.length() - 1; i++) {
+		for (int i = 0; i < leftStrand.length(); i++) {
 			if (leftStrand.charAt(i) != rightStrand.charAt(i))
 				hammingDistance++;
 		}
