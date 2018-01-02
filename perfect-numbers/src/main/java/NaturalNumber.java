d84b0277fd502f6598b64aa326a6d15db493997a
Zack Hartmann <zackhartmann@gmail.com>
2018-01-01 20:42:24 -0500

    Implemented Throw Exception

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,5 +1,32 @@
 class NaturalNumber {
 
+  private int number;
+  private int aliquot;
 
+  NaturalNumber(int number) {
+    // throw exception for illegal arguments
+    if (number <= 0)
+      throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+    // define object values
+    this.number = number;
+    this.aliquot = 0;
+  }
+
+  Classification getClassification() {
+    // resolve the aliquot sum
+    for (int i = number/2; i > 0; i--) {
+      if (number % i == 0)
+        aliquot += i;
+    }
+
+    if (aliquot == number)
+      return Classification.PERFECT;
+
+    if (aliquot > number)
+      return Classification.ABUNDANT;
+
+    // if (aliquot < number) (implied)
+    return Classification.DEFICIENT;
+  }
 
 }
