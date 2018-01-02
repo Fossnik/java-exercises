e305def3866fcc1be1e3906674d39741396b7479
Zack Hartmann <zackhartmann@gmail.com>
2018-01-02 12:41:02 -0500

    fullProverb Test Passed

--- proverb/src/main/java/Proverb.java
+++ proverb/src/main/java/Proverb.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Proverb {
 
 	// string array to be defined by Proverb(words)
@@ -10,8 +13,7 @@ class Proverb {
 "For want of a horse the rider was lost.",
 "For want of a rider the message was lost.",
 "For want of a message the battle was lost.",
-"For want of a battle the kingdom was lost.",
-"And all for the want of a horseshoe nail."
+"For want of a battle the kingdom was lost."
 	};
 
 	Proverb(String[] words) {
@@ -22,23 +24,38 @@ class Proverb {
 		// build output String
 		String proverbialOutput = new String("");
 
+		// keep track of used proverbs to avoid repeating
+		List<Integer> used = new ArrayList<Integer>();
+
 		// iterate through input words
 		for (int i = 0; i < werdz.length; i++) {
 			// search for given input word in each proverb
-			for (int j = 0; i < proverbs.length; j++) {
+			for (int j = 0; j < proverbs.length; j++) {
 				// add a proverb to output string if it is found.
 				if (proverbs[j].indexOf(werdz[i]) != -1) {
-					// a newline character is required sometimes
-					if (proverbialOutput.length() > 0)
-						proverbialOutput += "\n";
+					// don't repeat previously used proverbs
+					if (!(used.contains(j))) {
+						// a newline character preface is required, but not initially
+						if (proverbialOutput.length() > 0)
+							proverbialOutput += "\n";
 
-					proverbialOutput += proverbs[j];
+						proverbialOutput += proverbs[j];
+
+						// avoid repeating the proverb
+						used.add(j);
+
+						// don't match more than one.
+						break;
+					}
 
 				}
 
 			}
 
 		}
+		// the last sentence will reiterate the first
+		if (werdz.length != 0)
+			proverbialOutput += "\nAnd all for the want of a " + werdz[0] + ".";
 
 		return proverbialOutput;
 	}
