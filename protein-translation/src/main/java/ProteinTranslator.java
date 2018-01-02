9c6585d04fa4b308df05a9356d97daaff4ab793c
Zack Hartmann <zackhartmann@gmail.com>
2018-01-02 10:49:12 -0500

    Implemented Switch Statement

--- protein-translation/src/main/java/ProteinTranslator.java
+++ protein-translation/src/main/java/ProteinTranslator.java
@@ -3,13 +3,51 @@ import java.util.ArrayList;
 
 class ProteinTranslator {
 
-	private ArrayList<String> protein = new ArrayList<String>();
-
 	List<String> translate(String rnaSequence) {
 
-		if (rnaSequence == "AUG")
-			protein.add("Methionine");
+		ArrayList<String> protein = new ArrayList<String>();
+		String codon;
+
+		// process rnaSequence string by 3 char increments
+		for (int offset = 0; offset < rnaSequence.length(); offset += 3) {
+			codon = rnaSequence.substring(offset, offset + 3);
+
+			switch (codon) {
+				case "AUG":
+					protein.add("Methionine");
+					break;
+
+				case "UUU": case "UUC":
+					protein.add("Phenylalanine");
+					break;
+
+				case "UUA": case "UUG":
+					protein.add("Leucine");
+					break;
+
+				case "UCU": case "UCC": case "UCA": case "UCG":
+					protein.add("Serine");
+					break;
+
+				case "UAU": case "UAC":
+					protein.add("Tyrosine");
+					break;
+
+				case "UGU": case "UGC":
+					protein.add("Cysteine");
+					break;
+
+				case "UGG":
+					protein.add("Tryptophan");
+					break;
+
+				case "UAA": case "UAG": case "UGA":
+					return protein; // STOP
+			}
+
+		}
 
 		return protein;
 	}
+
 }
