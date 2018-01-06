73819ada6c2856dcd44ccfbcb170007453cafaee
Zack Hartmann <zackhartmann@gmail.com>
2018-01-05 22:16:16 -0500

    Implemented Dynamic Set Testing

--- sum-of-multiples/src/main/java/SumOfMultiples.java
+++ sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,14 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class SumOfMultiples {
 
-	int sum = 0;
+	private int sum = 0;
+
+	// avoid summing the same number twice with this array
+	ArrayList<Integer> multiples = new ArrayList<Integer>();
 
 	SumOfMultiples(int number, int[] set) {
 
-		for (int i = 1; i < number; i++) {
+		for (int numerator = 1; numerator < number; numerator++) {
+	    for (int denominator = 0; denominator < set.length; denominator++) {
 
-			if (i % 3 == 0 || i % 5 == 0)
-				sum++;
+				if (numerator % set[denominator] == 0) {
+					if (multiples.indexOf(numerator) == -1) {
+						multiples.add(numerator);
+						sum += numerator;
+					}
+				}
 
+			}
 		}
 
 	}
