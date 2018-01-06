ce3bfa402ce31928d0c0f85aeb1651ef5a08f0ae
Zack Hartmann <zackhartmann@gmail.com>
2018-01-05 21:01:59 -0500

    Logic Proving

--- sum-of-multiples/src/main/java/SumOfMultiples.java
+++ sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,20 @@
 class SumOfMultiples {
 
-    SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int sum = 0;
 
-    int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	SumOfMultiples(int number, int[] set) {
+
+		for (int i = 1; i < number; i++) {
+
+			if (i % 3 == 0 || i % 5 == 0)
+				sum++;
+
+		}
+
+	}
+
+	int getSum() {
+			return sum;
+	}
 
 }
