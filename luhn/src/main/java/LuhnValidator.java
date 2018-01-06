47768e04b3d841f09b688b3f6fcef863a7bdbe44
Zack Hartmann <zackhartmann@gmail.com>
2018-01-06 00:39:22 -0500

    Implemented a Stringbuilder Reversal Method

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,21 @@
 class LuhnValidator {
 
-    boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isValid(String candidate) {
+		int sum = 0;
+		Boolean odd = false;
+
+		for (char c : new StringBuilder(candidate).reverse().toString().toCharArray()) {
+			if (Character.isDigit(c))
+				if ((odd =! odd) == true)
+					sum += Character.getNumericValue(c);
+				else
+					if (Character.getNumericValue(c) * 2 > 9)
+						sum += Character.getNumericValue(c) * 2 - 9;
+					else
+						sum += Character.getNumericValue(c) * 2;
+		}
+
+		return sum % 10 == 0;
+	}
 
 }
