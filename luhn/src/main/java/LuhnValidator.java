1b0b9417aeede0e3a052a24fcff296c09a26549b
Zack Hartmann <zackhartmann@gmail.com>
2018-01-06 00:59:37 -0500

    Accounted for Zero Quantity Discrepency

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -6,14 +6,20 @@ class LuhnValidator {
 
 		for (char c : new StringBuilder(candidate).reverse().toString().toCharArray()) {
 			if (Character.isDigit(c))
-				if ((odd =! odd) == true)
+				if (odd =! odd) // test and flip
 					sum += Character.getNumericValue(c);
 				else
 					if (Character.getNumericValue(c) * 2 > 9)
 						sum += Character.getNumericValue(c) * 2 - 9;
 					else
 						sum += Character.getNumericValue(c) * 2;
+				else if (!Character.isSpace(c))
+					return false;
 		}
+
+		// not clear why " 00000" should be valid and " 0" invalid, however...
+		if (sum == 0 && candidate.length() < 6)
+			return false;
 
 		return sum % 10 == 0;
 	}
