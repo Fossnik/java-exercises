406e546d2cb2d0afa7f56e73cccdb61f58f73745
Zack Hartmann <zackhartmann@gmail.com>
2018-01-07 16:59:15 -0500

    Implemented Stringbuilder

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,11 +1,5 @@
 class ReverseString {
-		String reverse(String inputString) {
-			String reversed = new String("");
-
-			for (int i = inputString.length() - 1; i >= 0; i--) {
-			  reversed += inputString.charAt(i);
-			}
-
-			return reversed;
-		}
+	String reverse(String inputString) {
+		return new StringBuilder(inputString).reverse().toString();
+	}
 }
