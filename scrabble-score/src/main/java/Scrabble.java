69eea611b5df8fd7b6dc72c65636642f8fb1bceb
Zack Hartmann <zackhartmann@gmail.com>
2018-01-07 17:36:26 -0500

    Moved Operations to Class Definition

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -11,38 +11,36 @@ class Scrabble {
 	Q, Z                               10
 **/
 
-	private String word;
+	private int wordScore = 0;
 
 	Scrabble(String word) {
-		this.word = word.toUpperCase();
+		word = word.toUpperCase();
+		// loop through each letter and tally
+		// note the lack of break statements.
+		for (char c : word.toCharArray()) {
+			switch (c) {
+				case 'Q': case 'Z':
+				wordScore += 2; // 10
+				case 'J': case 'X':
+				wordScore += 3; // 8
+				case 'K':
+				wordScore++; // 5
+				case 'F': case 'H': case 'V': case 'W': case 'Y':
+				wordScore++; // 4
+				case 'B': case 'C': case 'M': case 'P':
+				wordScore++; // 3
+				case 'D': case 'G':
+				wordScore++; // 2
+				case 'A': case 'E': case 'I': case 'O': case 'U':
+				case 'L': case 'N':	case 'R': case 'S': case 'T':
+				wordScore++; // 1
+				default:
+				continue;
+			}
+		}
 	}
 
 	int getScore() {
-		int wordScore = 0;
-		// loop through each letter and tally
-		// note the lack of break statements.
-		for (int i = 0; i <= word.length() - 1; i++) {
-			switch (word.charAt(i)) {
-				case 'Q': case 'Z':
-					wordScore += 2; // 10
-				case 'J': case 'X':
-					wordScore += 3; // 8
-				case 'K':
-					wordScore++; // 5
-				case 'F': case 'H': case 'V': case 'W': case 'Y':
-					wordScore++; // 4
-				case 'B': case 'C': case 'M': case 'P':
-					wordScore++; // 3
-				case 'D': case 'G':
-					wordScore++; // 2
-				case 'A': case 'E': case 'I': case 'O': case 'U':
-				case 'L': case 'N':	case 'R': case 'S': case 'T':
-					wordScore++; // 1
-				default:
-					continue;
-			}
-		}
-
 		return wordScore;
 	}
 
