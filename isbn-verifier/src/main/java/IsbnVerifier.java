16bae001cff9d7970be1d58a6e29241557e31b45
Zack Hartmann <zackhartmann@gmail.com>
2018-01-08 13:31:35 -0500

    Implemented ISBN Verification

--- isbn-verifier/src/main/java/IsbnVerifier.java
+++ isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,7 +1,13 @@
 class IsbnVerifier {
 
-    boolean isValid(String stringToVerify) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isValid(String stringToVerify) {
+		int checkdigit = 10, sum = 0;
+
+		for (char c : stringToVerify.toCharArray())
+			if (Character.isDigit(c))
+				sum += Character.getNumericValue(c) * checkdigit--;
+
+		return sum % 11 == 0;
+	}
 
 }
