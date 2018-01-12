13dc1e3a38d0187fd0eac8c8b43a8da11907ba94
Zack Hartmann <zackhartmann@gmail.com>
2018-01-11 19:56:11 -0500

    Implemented Triangle Testing

--- triangle/src/main/java/Triangle.java
+++ triangle/src/main/java/Triangle.java
@@ -1,19 +1,23 @@
 class Triangle {
 
-    Triangle(double side1, double side2, double side3) throws TriangleException {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	public double side1, side2, side3;
 
-    boolean isEquilateral() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Triangle(double side1, double side2, double side3) throws TriangleException {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
 
-    boolean isIsosceles() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isEquilateral() {
+		return side1 == side2 && side2 == side3;
+	}
 
-    boolean isScalene() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isIsosceles() {
+		return side1 == side2 || side2 == side3 || side1 == side3;
+	}
+
+	boolean isScalene() {
+		return side1 != side2 && side2 != side3 && side1 == side3;
+	}
 
 }
