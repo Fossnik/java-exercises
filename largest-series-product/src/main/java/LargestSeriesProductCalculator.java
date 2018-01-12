af5f1f262cb283070d5f6e78a7fe4204de4b0c10
Zack Hartmann <zackhartmann@gmail.com>
2018-01-12 13:52:36 -0500

    Implemented Foundational Logic

--- largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,9 +1,30 @@
 class LargestSeriesProductCalculator {
-    LargestSeriesProductCalculator(String inputNumber) {
-        throw new UnsupportedOperationException ("Delete this statement and write your own implementation.");
-    }
 
-    long calculateLargestProductForSeriesLength(int numberOfDigits) {
-        throw new UnsupportedOperationException ("Delete this statement and write your own implementation.");
-    }
+	private String inputNumber;
+
+	LargestSeriesProductCalculator(String inputNumber) {
+		this.inputNumber = inputNumber;
+	}
+
+	long calculateLargestProductForSeriesLength(int numberOfDigits) {
+
+		long largest = 0L;
+
+		// iterate through each grouping of digits
+		for (int i = numberOfDigits; i <= inputNumber.length(); i++) {
+
+			// find the product of the group of digits
+			long product = 1L;
+			for (int n = numberOfDigits; n > 0; n--)
+				product *= Character.getNumericValue(inputNumber.charAt(i - n));
+
+			// keep track of the largest product
+			if (product > largest)
+				largest = product;
+
+		}
+
+		return largest;
+	}
+
 }
