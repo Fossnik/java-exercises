5b670648b45b2e95d8d3dc272d57a67b8c672edb
Zack Hartmann <zackhartmann@gmail.com>
2018-01-18 19:15:57 -0500

    Implemented Revised End Verse Logic

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,13 +1,41 @@
 class TwelveDays {
-    String verse(int verseNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
-    String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-    
-    String sing() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	private String[] ordinals = new String[] {"zeroeth", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"};
+
+	String verse(int verseNumber) {
+		String v = new String("On the " + ordinals[verseNumber] + " day of Christmas my true love gave to me, ");
+
+		switch(verseNumber) {
+			case 12: 	v += "twelve Drummers Drumming, ";
+			case 11: 	v += "eleven Pipers Piping, ";
+			case 10: 	v += "ten Lords-a-Leaping, ";
+			case 9:		v += "nine Ladies Dancing, ";
+			case 8:		v += "eight Maids-a-Milking, ";
+			case 7:		v += "seven Swans-a-Swimming, ";
+			case 6:		v += "six Geese-a-Laying, ";
+			case 5:		v += "five Gold Rings, ";
+			case 4:		v += "four Calling Birds, ";
+			case 3:		v += "three French Hens, ";
+			case 2:		v += "two Turtle Doves, ";
+			case 1:
+				if (verseNumber == 1) v += "a Partridge in a Pear Tree.\n";
+				else v += "and a Partridge in a Pear Tree.\n";
+		}
+
+		return v;
+	}
+
+	String verses(int startVerse, int endVerse) {
+		String vs = new String("");
+
+		while (startVerse < endVerse)
+			vs += verse(startVerse++) + "\n";
+
+		return vs + verse(endVerse);
+	}
+
+	String sing() {
+		return verses(1, 12);
+	}
+
 }
