c6bd696ba19f264635a2a7c7c8f9eec8db147a60
Zack Hartmann <zackhartmann@gmail.com>
2018-01-28 17:29:00 -0500

    Implemented HashSet Structure

--- sum-of-multiples/src/main/java/SumOfMultiples.java
+++ sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,26 +1,22 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 class SumOfMultiples {
 
 	private int sum = 0;
 
-	// avoid summing the same number twice with this array
-	ArrayList<Integer> multiples = new ArrayList<Integer>();
+	// avoid summing the same number twice using this HashSet (hashset implies non-duplication)
+	Set<Integer> multiples = new HashSet<Integer>();
 
 	SumOfMultiples(int number, int[] set) {
 
-		for (int numerator = 1; numerator < number; numerator++) {
-			for (int denominator : set) {
-
-				if (numerator % denominator == 0 && multiples.indexOf(numerator) == -1) {
+		for (int numerator = 1; numerator < number; numerator++)
+			for (int denominator : set)
+				if (numerator % denominator == 0)
 					multiples.add(numerator);
-					sum += numerator;
-				}
 
-			}
-		}
-
+		for (int i : multiples)
+			sum+=i;
 	}
 
 	int getSum() {
