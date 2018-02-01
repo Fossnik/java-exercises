673b5d5fd9a8de2b2d89940a7c409591d325c9e4
Zack Hartmann <zackhartmann@gmail.com>
2018-02-01 11:52:44 -0500

    Implemented Sieve of Eratosthenes

--- sieve/src/main/java/Sieve.java
+++ sieve/src/main/java/Sieve.java
@@ -4,25 +4,20 @@ import java.util.ArrayList;
 class Sieve {
 
 	private int maxPrime;
+	private List<Integer> primes = new ArrayList<Integer>();
 
 	Sieve(int maxPrime) {
-		this.maxPrime = maxPrime;
-	}
+		boolean[] notPrime = new boolean[maxPrime+1];
 
-	List<Integer> getPrimes() {
-		List<Integer> primes = new ArrayList<Integer>();
+		for (int i = 2; i < Math.sqrt(maxPrime); i++)
+			if (notPrime[i] == false)
+				for (int j = i * i; j <= maxPrime; j += i)
+					notPrime[j] = true;
 
-		for (int i = 2; i <= maxPrime; i++) {
-			boolean isPrime = true;
-			for (int j = 2; j <= i / 2; j++)
-				if (i % j == 0) {
-					isPrime = false;
-					break;
-				}
-			if (isPrime == true)
+		for (int i = 2; i < notPrime.length; i++)
+			if (notPrime[i] == false)
 				primes.add(i);
-		}
-
-		return primes;
 	}
+
+	List<Integer> getPrimes() { return primes; }
 }
