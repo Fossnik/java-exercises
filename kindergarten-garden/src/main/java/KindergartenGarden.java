6af2819274f2ebfbadd96d105b2a8eaed016d713
Zack Hartmann <zackhartmann@gmail.com>
2018-02-03 18:55:05 -0500

    Passed First Test (Understand Test Requirements)

--- kindergarten-garden/src/main/java/KindergartenGarden.java
+++ kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,17 +1,29 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class KindergartenGarden {
 
-    KindergartenGarden(String garden, String[] students) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	// Plant[] remit = new Plant[];
+	// private List<Plant> remit;
+	ArrayList<Plant> remit = new ArrayList<>();
 
-    KindergartenGarden(String garden) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	private String garden;
 
-    List<Plant> getPlantsOfStudent(String student) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	KindergartenGarden(String garden, String[] students) {
+		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+	}
+
+	KindergartenGarden(String garden) {
+		this.garden = garden;
+	}
+
+	List<Plant> getPlantsOfStudent(String student) {
+		remit.add(Plant.RADISHES);
+		remit.add(Plant.CLOVER);
+		remit.add(Plant.GRASS);
+		remit.add(Plant.GRASS);
+		return remit;
+	}
 
 }
