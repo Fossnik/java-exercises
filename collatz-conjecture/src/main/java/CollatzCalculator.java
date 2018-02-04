c70c2b6c91d94cbc85424151b8d1de959e015290
Zack Hartmann <zackhartmann@gmail.com>
2018-02-04 08:55:10 -0500

    All Tests Passing

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -2,6 +2,9 @@ class CollatzCalculator {
 
 	int computeStepCount(int start) {
 
+		if (start < 1)
+			throw new IllegalArgumentException("Only natural numbers are allowed");
+
 		int steps = 0;
 		int n = start;
 
