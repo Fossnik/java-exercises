bca6c8e2a3c89d148292e5f24204fc3eabff8084
Zack Hartmann <zackhartmann@gmail.com>
2018-02-04 10:29:17 -0500

    Implemented Prime Logic

--- nth-prime/src/main/java/PrimeCalculator.java
+++ nth-prime/src/main/java/PrimeCalculator.java
@@ -1,19 +1,25 @@
 class PrimeCalculator {
 
   int nth(int nth) {
+    if (nth < 1)
+      throw new IllegalArgumentException("negative");
 
+    int numPrimes = 0;
     int lastP = 0;
     boolean isP = true;
 
-    for (int i = 2; lastP < nth; i++) {
-      for (int j = 2; j <= i / 2; j++)
-        if (i % j == 0) {
-          isP = false;
-          break;
-        }
-          
-      if (isP)
-        lastP = i;
+    for (int over = 2; numPrimes < nth; over++) {
+			isP = true;
+			for (int under = 2; under <= over / 2; under++)
+				if (over % under == 0) {
+					isP = false;
+					break;
+				}
+
+      if (isP) {
+        numPrimes++;
+        lastP = over;
+      }
     }
 
     return lastP;
