c21134464928c370c44408f3fca4869d4ccaf8f4
Zack Hartmann <zackhartmann@gmail.com>
2018-02-04 09:13:12 -0500

    Improved Code Profile

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -2,15 +2,12 @@ class CollatzCalculator {
 
 	int computeStepCount(int start) {
 
-		if (start < 1)
-			throw new IllegalArgumentException("Only natural numbers are allowed");
+		if (start < 1) new IllegalArgumentException("Only natural numbers are allowed");
 
 		int steps = 0;
 		for (int n = start; n != 1; steps++)
-			if (n % 2 == 0)
-				n = n / 2;
-			else
-				n = n * 3 + 1;
+			if (n % 2 == 0) n = n / 2;
+			else n = n * 3 + 1;
 
 		return steps;
 	}
