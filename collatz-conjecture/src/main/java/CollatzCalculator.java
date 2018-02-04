14b4da48bd21c8cc9d7d7cce42be9f34ccdd21ba
Zack Hartmann <zackhartmann@gmail.com>
2018-02-04 08:58:53 -0500

    Implemented for loop logic

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -6,16 +6,11 @@ class CollatzCalculator {
 			throw new IllegalArgumentException("Only natural numbers are allowed");
 
 		int steps = 0;
-		int n = start;
-
-		while (n != 1) {
+		for (int n = start; n != 1; steps++)
 			if (n % 2 == 0)
 				n = n / 2;
 			else
 				n = n * 3 + 1;
-
-			steps++;
-		}
 
 		return steps;
 	}
