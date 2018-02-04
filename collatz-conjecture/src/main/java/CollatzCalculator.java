161c190118deb542b5118309cf8a6d578ffd1acb
Zack Hartmann <zackhartmann@gmail.com>
2018-02-04 09:48:51 -0500

    First Test Passed

--- collatz-conjecture/src/main/java/CollatzCalculator.java
+++ collatz-conjecture/src/main/java/CollatzCalculator.java
@@ -6,7 +6,7 @@ class CollatzCalculator {
 
 		int steps = 0;
 		for (int n = start; n != 1; steps++)
-			if (n % 2 == 0) n = n / 2;
+			if (n % 2) n = n / 2;
 			else n = n * 3 + 1;
 
 		return steps;
--- nth-prime/src/main/java/PrimeCalculator.java
+++ nth-prime/src/main/java/PrimeCalculator.java
@@ -1,7 +1,22 @@
 class PrimeCalculator {
 
   int nth(int nth) {
-    throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+    int lastP = 0;
+    boolean isP = true;
+
+    for (int i = 2; lastP < nth; i++) {
+      for (int j = 2; j <= i / 2; j++)
+        if (i % j == 0) {
+          isP = false;
+          break;
+        }
+          
+      if (isP)
+        lastP = i;
+    }
+
+    return lastP;
   }
 
 }
