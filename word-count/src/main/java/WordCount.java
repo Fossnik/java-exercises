214a9f671c354e848977b3c89bd66f07e05c9ff9
Zack Hartmann <zackhartmann@gmail.com>
2018-02-06 12:29:12 -0500

    Implemented Counting Logic

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -12,8 +12,11 @@ class WordCount {
 	// this is the method
 	public Map<String, Integer> phrase(String input) {
 
-		for (String s : input.toLowerCase().split(" "))
-			wc.put(s, 1);
+		for (String s : input.toLowerCase().split("[^A-z0-9]"))
+			if (wc.get(s) != null)
+				wc.put(s, wc.get(s) + 1);
+			else
+				wc.put(s, 1);
 
 		return wc;
 	}
