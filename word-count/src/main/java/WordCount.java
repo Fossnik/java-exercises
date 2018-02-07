e4ee8dd0e5888e76fb365354f5a5abb7c464a934
Zack Hartmann <zackhartmann@gmail.com>
2018-02-06 22:09:41 -0500

    Resolved Structure

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -4,12 +4,15 @@ import java.util.Map;
 class WordCount {
 
 	public Map<String, Integer> phrase(String input) {
+
 		Map<String, Integer> tally = new HashMap<>();
+
 		// remediate words in quotes (lousy workaround)
 		input = input.replace("'large'", "large");
 
 		// split around non-words, excempting apostrophes
-		for (String s : input.toLowerCase().split("(\\\n|(?!')\\W)+"))
+		String regEx = "(\\\n|(?!')\\W)+";
+		for (String s : input.toLowerCase().split(regEx))
 			if (tally.get(s) != null)
 				tally.put(s, tally.get(s) + 1);
 			else
