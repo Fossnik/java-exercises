3eb62f72814bc4dfa773b6435bb1e5dcad86bb81
Zack Hartmann <zackhartmann@gmail.com>
2018-02-11 17:40:07 -0500

    First Test - Understanding Return Structure

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -1,9 +1,14 @@
 import java.util.List;
+import java.util.ArrayList;
 
 class DiamondPrinter {
 
   List<String> printToList(char a) {
-    
+    List<String> remit = new ArrayList<String>();
+
+    remit.add(String.valueOf(a));
+
+    return remit;
   }
 
 }
