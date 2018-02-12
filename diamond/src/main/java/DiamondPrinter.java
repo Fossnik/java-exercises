fd434d7c6c3a64f37dd1c19ee463600cc14d2c26
Zack Hartmann <zackhartmann@gmail.com>
2018-02-12 00:32:40 -0500

    Implemented Logic

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -3,12 +3,33 @@ import java.util.ArrayList;
 
 class DiamondPrinter {
 
-  List<String> printToList(char a) {
-    List<String> remit = new ArrayList<String>();
+  private static List<String> remit = new ArrayList<String>();
+  private static int depth;
+  private static char max;
 
-    remit.add(String.valueOf(a));
+  static List<String> printToList(char a) {
+
+    max = (char) (a - 'A');
+    depth = max * 2 + 1;
+
+    for (char i = 'A'; i < a; i++)
+      remit.add(printRow(i));
+
+    for (char i = a; i >= 'A'; i--)
+      remit.add(printRow(i));
 
     return remit;
+  }
+
+  private static String printRow(char a) {
+    String row = "";
+    for (int i = 0; i < depth; i++)
+      if (i == max - (a-'A') || i == a-'A' + max)
+        row += a;
+      else
+        row += ' ';
+
+    return row;
   }
 
 }
