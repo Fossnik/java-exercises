3960924c7cb8d6ea1cf7b7e0cb8f1b47ee991c74
Zack Hartmann <zackhartmann@gmail.com>
2018-02-12 20:36:52 -0500

    Added Negative Lookahead to Catch Exceptions

--- pig-latin/src/main/java/PigLatinTranslator.java
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -3,10 +3,22 @@ class PigLatinTranslator {
   public PigLatinTranslator() {}
 
   public String translate(String word) {
-    if ("aeiou".contains(String.valueOf(word.charAt(0))))
+
+    String re = "^(ch|qu|the|rh).*";
+    if (word.matches(re))
+      return word.substring(2)+word.substring(0,2)+"ay";
+
+    re = "^(sch|squ|thr).*";
+    if (word.matches(re))
+      return word.substring(3)+word.substring(0,3)+"ay";
+
+    if ("aeiou".contains(String.valueOf(word.charAt(0))) ||
+        word.matches("^(x|y)(?!e).*"))
       return word+"ay";
 
-    return "";
+    return word.substring(1)+word.charAt(0)+"ay";
+
+    // return "";
   }
 
 }
