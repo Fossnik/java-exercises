0a09b6e1b10b6d86d1208e288491a57a35adbf99
Zack Hartmann <zackhartmann@gmail.com>
2018-02-12 20:49:29 -0500

    Implemented Exception for Multi-word Input

--- pig-latin/src/main/java/PigLatinTranslator.java
+++ pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,24 +1,28 @@
 class PigLatinTranslator {
 
-  public PigLatinTranslator() {}
+	public PigLatinTranslator() {}
 
-  public String translate(String word) {
+	public String translate(String word) {
 
-    String re = "^(ch|qu|the|rh).*";
-    if (word.matches(re))
-      return word.substring(2)+word.substring(0,2)+"ay";
+		if (word.split(" ").length > 1) {
+			String out = "";
+			for (String s : word.split(" "))
+				out += translate(s) + " ";
 
-    re = "^(sch|squ|thr).*";
-    if (word.matches(re))
-      return word.substring(3)+word.substring(0,3)+"ay";
+			return out.trim();
+		}
 
-    if ("aeiou".contains(String.valueOf(word.charAt(0))) ||
-        word.matches("^(x|y)(?!e).*"))
-      return word+"ay";
+		if (word.matches("^(ch|qu|the|rh).*")
+			return word.substring(2)+word.substring(0,2)+"ay";
 
-    return word.substring(1)+word.charAt(0)+"ay";
+		if (word.matches("^(sch|squ|thr).*"))
+			return word.substring(3)+word.substring(0,3)+"ay";
 
-    // return "";
-  }
+		if ("aeiou".contains(String.valueOf(word.charAt(0))) ||
+				word.matches("^(x|y)(?!e).*"))
+			return word+"ay";
+
+		return word.substring(1)+word.charAt(0)+"ay";
+	}
 
 }
