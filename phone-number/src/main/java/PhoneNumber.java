1e3c7531ab0034becbd7d0ebb180b4d4075fdf77
Zack Hartmann <zackhartmann@gmail.com>
2018-02-13 11:49:08 -0500

    Implemented Illegal Argument Exceptions

--- phone-number/src/main/java/PhoneNumber.java
+++ phone-number/src/main/java/PhoneNumber.java
@@ -1,15 +1,27 @@
 class PhoneNumber {
 
-  public String phoneNumber;
+	private String digits;
 
-  public PhoneNumber(String input) {
-    this.phoneNumber = input;
-  }
+	public PhoneNumber(String input) {
+		if (! input.matches("[\\d. )(\"+-]*"))
+			throw new IllegalArgumentException("Illegal character in phone number. Only digits, spaces, parentheses, hyphens or dots accepted.");
 
-  public String getNumber() {
-    String digits = this.phoneNumber.replaceAll("\\D", "");
+		this.digits = input.replaceAll("\\D", "");
+		int length = this.digits.length();
 
+		if (!(length == 10 || length == 11))
+				throw new IllegalArgumentException("Number must be 10 or 11 digits");
+
+		if (digits.substring(0,length - 9).matches("[0-1]") ||
+			digits.substring(length - 7, length - 6).matches("[0-1]"))
+			throw new IllegalArgumentException("Illegal Area Or Exchange Code. Only 2-9 are valid digits");
+
+		if (length == 11 && !this.digits.startsWith("1"))
+			throw new IllegalArgumentException("Can only have 11 digits if number starts with '1'");
+	}
+
+	public String getNumber() {
 		return digits.substring(digits.length() - 10, digits.length());
-  }
+	}
 
 }
