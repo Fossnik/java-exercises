52d06d2ea272d336a940c3157ea5adec59b02034
Zack Hartmann <zackhartmann@gmail.com>
2018-02-13 17:54:26 -0500

    Implemented Illegal Argument Exception

--- nucleotide-count/src/main/java/NucleotideCounter.java
+++ nucleotide-count/src/main/java/NucleotideCounter.java
@@ -3,28 +3,25 @@ import java.util.HashMap;
 
 class NucleotideCounter {
 
-  private String strand;
   private HashMap<Character, Integer> counts = new HashMap<>();
 
   NucleotideCounter(String strand) {
-    this.strand = strand;
-  }
-
-  public HashMap<Character, Integer> nucleotideCounts() {
     int A = 0, C = 0, G = 0, T = 0;
 
-    for (Character c : this.strand.toCharArray()) {
+    for (Character c : strand.toCharArray())
       if      (c.equals('A')) A++;
       else if (c.equals('C')) C++;
       else if (c.equals('G')) G++;
       else if (c.equals('T')) T++;
-    }
+      else throw new IllegalArgumentException("only A, C, G, or T");
 
-    counts.put('A', A);
-    counts.put('C', C);
-    counts.put('G', G);
-    counts.put('T', T);
+    this.counts.put('A', A);
+    this.counts.put('C', C);
+    this.counts.put('G', G);
+    this.counts.put('T', T);
+  }
 
+  public HashMap<Character, Integer> nucleotideCounts() {
     return counts;
   }
 
