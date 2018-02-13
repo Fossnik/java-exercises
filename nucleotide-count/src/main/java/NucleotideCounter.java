99916acde48eeb42d36fa6a3e8c055e6ff24aa39
Zack Hartmann <zackhartmann@gmail.com>
2018-02-13 17:45:07 -0500

    Implemented Counting Logic

--- nucleotide-count/src/main/java/NucleotideCounter.java
+++ nucleotide-count/src/main/java/NucleotideCounter.java
@@ -11,10 +11,19 @@ class NucleotideCounter {
   }
 
   public HashMap<Character, Integer> nucleotideCounts() {
-    counts.put('A', 0);
-    counts.put('C', 0);
-    counts.put('G', 0);
-    counts.put('T', 0);
+    int A = 0, C = 0, G = 0, T = 0;
+
+    for (Character c : this.strand.toCharArray()) {
+      if      (c.equals('A')) A++;
+      else if (c.equals('C')) C++;
+      else if (c.equals('G')) G++;
+      else if (c.equals('T')) T++;
+    }
+
+    counts.put('A', A);
+    counts.put('C', C);
+    counts.put('G', G);
+    counts.put('T', T);
 
     return counts;
   }
