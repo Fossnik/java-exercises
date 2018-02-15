818841b4f310a5a39a07d5a64415e86e36c58641
Zack Hartmann <zackhartmann@gmail.com>
2018-02-15 13:23:30 -0500

    Implemented Encode Logic

--- run-length-encoding/src/main/java/RunLengthEncoding.java
+++ run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -1,8 +1,32 @@
 class RunLengthEncoding {
 
   public String encode(String input) {
-    return input;
-  }
+    StringBuilder output = new StringBuilder();
+
+		char a = '\r';
+		char b;
+    int i = 0;
+		int count = 1;
+		while (!input.isEmpty()) {
+			try {
+				a = input.charAt(i);
+				b = input.charAt(++i);
+
+				if (a == b) {
+					count++;
+				} else {
+					output.append((count > 1 ? (Integer.toString(count) + a) : a));
+					count = 1;
+				}
+
+			} catch (Exception e) {
+        output.append((count > 1 ? (Integer.toString(count) + a) : a));
+        break;
+      }
+		}
+
+		return output.toString();
+	}
 
   public String decode(String input) {
     return input;
