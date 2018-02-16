6a6260da69fd530a87975f2781e827834667059c
Zack Hartmann <zackhartmann@gmail.com>
2018-02-15 23:39:26 -0500

    Implemented StringBuilder Class

--- run-length-encoding/src/main/java/RunLengthEncoding.java
+++ run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -1,11 +1,10 @@
 class RunLengthEncoding {
 
-  public String encode(String input) {
-    StringBuilder output = new StringBuilder();
-
+	public static String encode(String input) {
+		StringBuilder output = new StringBuilder();
 		char a = '\r';
 		char b;
-    int i = 0;
+		int i = 0;
 		int count = 1;
 		while (!input.isEmpty()) {
 			try {
@@ -18,39 +17,33 @@ class RunLengthEncoding {
 					output.append((count > 1 ? (Integer.toString(count) + a) : a));
 					count = 1;
 				}
-
 			} catch (Exception e) {
-        output.append((count > 1 ? (Integer.toString(count) + a) : a));
-        break;
-      }
+				output.append((count > 1 ? (Integer.toString(count) + a) : a));
+				break;
+			}
 		}
 
 		return output.toString();
 	}
 
-  public String decode(String input) {
-    StringBuilder output = new StringBuilder("");
-		char[] ca = input.toCharArray();
-		String runLength;
-
+	public static String decode(String input) {
+		StringBuilder output = new StringBuilder();
 		int i = 0;
-		while (!input.isEmpty()) {
-			runLength = "";
-			try {
-				if (Character.isDigit(ca[i]))
-					while (Character.isDigit(ca[i]))
-						runLength += ca[i++];
-				else
-					runLength = "1";
+		while (!input.isEmpty()) try {
+			String prefix = "";
+			while (Character.isDigit(input.charAt(i)))
+				prefix += input.charAt(i++);
 
-				for (int j = Integer.valueOf(runLength); j > 0; j--)
-					output.append(ca[i]);
+			int runLength = (prefix.equals("") ? 1 : Integer.valueOf(prefix));
+			while (runLength-- > 0)
+				output.append(input.charAt(i));
 
-				i++;
-			} catch (Exception e) {	break; }
+			i++;
+		} catch (Exception e) {
+			break;
 		}
 
 		return output.toString();
-  }
+	}
 
 }
