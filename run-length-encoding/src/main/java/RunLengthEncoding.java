3fac35cc4a87be0f3a5c20b812655eaf0e339c3e
Zack Hartmann <zackhartmann@gmail.com>
2018-02-15 23:45:19 -0500

    Revision - Java Inline Style

--- run-length-encoding/src/main/java/RunLengthEncoding.java
+++ run-length-encoding/src/main/java/RunLengthEncoding.java
@@ -6,21 +6,19 @@ class RunLengthEncoding {
 		char b;
 		int i = 0;
 		int count = 1;
-		while (!input.isEmpty()) {
-			try {
-				a = input.charAt(i);
-				b = input.charAt(++i);
+		while (!input.isEmpty()) try {
+			a = input.charAt(i);
+			b = input.charAt(++i);
 
-				if (a == b) {
-					count++;
-				} else {
-					output.append((count > 1 ? (Integer.toString(count) + a) : a));
-					count = 1;
-				}
-			} catch (Exception e) {
+			if (a == b) {
+				count++;
+			} else {
 				output.append((count > 1 ? (Integer.toString(count) + a) : a));
-				break;
+				count = 1;
 			}
+		} catch (Exception e) {
+			output.append((count > 1 ? (Integer.toString(count) + a) : a));
+			break;
 		}
 
 		return output.toString();
