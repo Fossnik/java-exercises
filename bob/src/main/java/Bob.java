2094dcbc50a834b81543d2e6f3e0f84c1d7da146
Zack Hartmann <zackhartmann@gmail.com>
2018-02-21 11:15:42 -0500

    Implemented isShouting Class

--- bob/src/main/java/Bob.java
+++ bob/src/main/java/Bob.java
@@ -1,7 +1,25 @@
 class Bob {
 
+	private String impetus;
+
 	public String hey(String input) {
+		impetus = input.trim();
+
+		if (isShouting())
+			return "Whoa, chill out!";
+
 		return "Whatever.";
+	}
+
+	private boolean isShouting() {
+		if (impetus.endsWith("!"))
+			return true;
+
+		// no lowercase characters
+		if (impetus.matches("[^a-z]+"))
+			return true;
+
+		return false;
 	}
 
 }
