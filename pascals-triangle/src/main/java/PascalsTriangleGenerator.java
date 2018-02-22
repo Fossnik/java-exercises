dfd06812990e7052fc81ae9c06207ef9683777e9
Zack Hartmann <zackhartmann@gmail.com>
2018-02-22 14:39:16 -0500

    Implemented Pascal Logic

--- pascals-triangle/src/main/java/PascalsTriangleGenerator.java
+++ pascals-triangle/src/main/java/PascalsTriangleGenerator.java
@@ -1,18 +1,28 @@
 class PascalsTriangleGenerator {
 
-	private int[][] triangle = new int[][]{};
+	private int[][] triangle;
 	private int size;
 
 	public int[][] generateTriangle(int size) {
 		this.size = size;
+		this.triangle = new int[size][size];
+
 		for (int i = 0; i < size; i++)
-			makeRow(i);
+			makeColumn(i);
 
 		return triangle;
 	}
 
-	private void makeRow(int n) {
-		System.out.println(n);
+	private void makeColumn(int x) {
+
+		for (int y = x; y < size; y++) {
+			try {
+				triangle[y][x] = triangle[y - 1][x] + triangle[y - 1][x - 1];
+			} catch (ArrayIndexOutOfBoundsException e) {
+				triangle[y][x] = 1;
+			}
+		}
+
 	}
 
 }
