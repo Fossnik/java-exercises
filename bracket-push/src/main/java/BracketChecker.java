8f93d35d1d546e57f21c59918ecda82dc7dfc649
Zack Hartmann <zackhartmann@gmail.com>
2018-02-25 18:04:11 -0500

    Implemented Stack Logic Solution

--- bracket-push/src/main/java/BracketChecker.java
+++ bracket-push/src/main/java/BracketChecker.java
@@ -7,59 +7,19 @@ class BracketChecker {
 	}
 
 	public boolean areBracketsMatchedAndNestedCorrectly() {
-		return input.isEmpty() || hasInternalSymmetry(input) && testString(this.input);
-	}
+		Stack<Character> stack = new Stack<>();
 
-	private boolean testString(String string) {
-
-		int end = opposingBracketIndex(string);
-		if (end == -1)
-			return false;
-
-		String substring = string.substring(1, end);
-
-		if (!hasInternalSymmetry(substring))
-			return false;
-		else
-			testString(substring);
-
-		return true;
-	}
-
-	private int opposingBracketIndex(String string) {
-		// returns the index of the opposite bracket.
-
-		if (string.length() < 2)
-			return -1;
-
-		char bracket = string.charAt(0);
-		char mirror = mirror(bracket);
-
-		if (mirror == '\0')
-			return -1;
-
-		// note the number of forward brackets -
-		// an additional forward bracket means searching past the first matching bracket
-		int index = 0;
-		int depth = 0;
-		while (index < string.length())
-			if (string.charAt(++index) == mirror)
-				if (depth == 0)
-					return index;
+		for (char c : input.toCharArray())
+			if (String.valueOf(c).matches("[\\{\\(\\[]"))
+				stack.add(c);
+			else
+				if (stack.isEmpty())
+					return false;
 				else
-					depth--;
-			else if (string.charAt(index) == bracket)
-				depth++;
+					if (c == mirror(stack.peek()))
+						stack.pop();
 
-		return -1;
-	}
-
-	private boolean hasInternalSymmetry (String s) {
-		// input has an equivalent quantity of the respective left and right brackets?
-		return
-				   s.replaceAll("\\[", "").length() == s.replaceAll("\\]", "").length()
-				&& s.replaceAll("\\(", "").length() == s.replaceAll("\\)", "").length()
-				&& s.replaceAll("\\{", "").length() == s.replaceAll("\\}", "").length();
+		return stack.isEmpty();
 	}
 
 	private char mirror(char bracket) {
