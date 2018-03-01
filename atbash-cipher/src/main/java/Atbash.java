14da7e3397619810edc93cb61de22c25248c27c1
Zack Hartmann <zackhartmann@gmail.com>
2018-03-01 16:24:13 -0500

    Made to Reflect Case-Insensitive Styling

--- atbash-cipher/src/main/java/Atbash.java
+++ atbash-cipher/src/main/java/Atbash.java
@@ -1,22 +1,33 @@
 class Atbash {
 
-	public String encode(String input) {
+	public static String encode(String input) {
 		String remit = "";
-		for (char c : input.toCharArray())
-			if (Character.isAlphabetic(c))
+		int index = 0;
+		for (char c : input.toCharArray()) {
+			if (index != 0 && index % 5 == 0)
+				remit += " ";
+
+			if (Character.isAlphabetic(c)) {
 				remit += switcheroo(c);
-			else
-				remit += c;
+				index++;
+			}
+		}
 
 		return remit;
 	}
 
-	public String decode(String input) {
-		return encode(input);
+	public static String decode(String input) {
+		String remit = "";
+		for (char c : input.toCharArray())
+			if (Character.isAlphabetic(c))
+				remit += switcheroo(c);
+
+		return remit;
 	}
 
-	private char switcheroo(char input) {
+	private static char switcheroo(char input) {
 		int zeroIndex = Character.toLowerCase(input) - 'a';
-		return (char)(Character.isLowerCase(input) ? 'z' - zeroIndex : 'Z' - zeroIndex);
+		return (char)('z' - zeroIndex);
 	}
+
 }
