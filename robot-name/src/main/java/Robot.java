26b2b93d853f2d81f455ebe1a3b443b3a6bcbd81
Zack Hartmann <zackhartmann@gmail.com>
2018-03-08 12:13:44 -0500

    Implemented a Coherant Object Model

--- robot-name/src/main/java/Robot.java
+++ robot-name/src/main/java/Robot.java
@@ -1,29 +1,29 @@
 import java.util.Random;
 import java.util.HashSet;
+import java.util.Set;
 
 class Robot {
 
 	private String name;
-	private static HashSet nameRegistrar = new HashSet<>();
+	private Set<String> nameRegistrar = new HashSet<>();
 
-	public Robot() {
-
-		do {
-			this.name = "";
-
-			Random r = new Random();
-
-			this.name += (char) (r.nextInt(26) + 'A');
-			this.name += (char) (r.nextInt(26) + 'A');
-
-			this.name += r.nextInt(10);
-			this.name += r.nextInt(10);
-			this.name += r.nextInt(10);
-
-		} while (nameRegistrar.contains(this.name));
-
+	Robot() {
+		this.name = generateName();
 		nameRegistrar.add(this.name);
+	}
 
+	private String generateName() {
+		StringBuilder possibleName = new StringBuilder("");
+		Random r = new Random();
+		do {
+			possibleName.append((char) (r.nextInt(26) + 'A'));
+			possibleName.append((char) (r.nextInt(26) + 'A'));
+			possibleName.append(r.nextInt(10));
+			possibleName.append(r.nextInt(10));
+			possibleName.append(r.nextInt(10));
+		} while (nameRegistrar.contains(possibleName.toString()));
+
+		return possibleName.toString();
 	}
 
 	public String getName() {
@@ -31,7 +31,8 @@ class Robot {
 	}
 
 	public void reset() {
-		this.name = new Robot().getName();
+		nameRegistrar.remove(this.name);
+		nameRegistrar.add(this.name = generateName());
 	}
 
 }
