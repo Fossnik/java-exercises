6896182742e07cd8f44e7cbd67338523540d8dab
Zack Hartmann <zackhartmann@gmail.com>
2018-03-21 14:17:49 -0400

    Implemented Two-Bucket Class

---
+++ two-bucket/src/main/java/TwoBucket.java
@@ -0,0 +1,13 @@
+class TwoBucket {
+	private int buck1size;
+	private int buck2size;
+	private int desiredLitres;
+	private String fillFirst;
+
+	TwoBucket(int buck1size, int buck2size, int desiredLitres, String fillFirst) {
+		this.buck1size = buck1size;
+		this.buck2size = buck2size;
+		this.desiredLitres = desiredLitres;
+		this.fillFirst = fillFirst;
+	}
+}
