225921bc60b80646b3b7bcd4c2ce12fe0ee01a98
Zack Hartmann <zackhartmann@gmail.com>
2018-04-28 01:57:04 -0400

    Git

--- two-bucket/src/main/java/TwoBucket.java
+++ two-bucket/src/main/java/TwoBucket.java
@@ -4,10 +4,25 @@ class TwoBucket {
 	private int desiredLitres;
 	private String fillFirst;
 
+	private int moves = 0;
+
 	TwoBucket(int buck1size, int buck2size, int desiredLitres, String fillFirst) {
 		this.buck1size = buck1size;
 		this.buck2size = buck2size;
 		this.desiredLitres = desiredLitres;
 		this.fillFirst = fillFirst;
 	}
+
+	public int getTotalMoves() {
+
+	}
+
+	public String getFinalBucket() {
+
+	}
+
+	public int getOtherBucket() {
+
+	}
+
 }
